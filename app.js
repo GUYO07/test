bd80f9cb60b9323cb45410f7839b46e74ea886dc
GUYO07 <worakan1489@gmail.com>
2023-05-18 13:30:20 +0700

    update

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const chalk = require('chalk');
-const debug = require('debug');
-const app = express();
-const port = 3000;
+const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
-app.use(morgan('combind'));
+const app = express();
+const port = process.env.PORT;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+
 app.get("/",(req,res)=>{
     res.send("Hello");
 })
 
 app.listen(port,()=>{
-    debug("Listening on port"+chalk.green(port));
+    debug("Listening on port"+chalk.green(" : "+port));
 })
